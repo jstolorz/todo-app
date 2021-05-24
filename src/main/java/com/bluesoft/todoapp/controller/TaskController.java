63c8e60730e4a8a62da7e19b4c2a3d4a6b4308ed
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-05-24 18:26:27 +0200

    Adding GetMapping with Pageable

--- src/main/java/com/bluesoft/todoapp/controller/TaskController.java
+++ src/main/java/com/bluesoft/todoapp/controller/TaskController.java
@@ -1,12 +1,17 @@
 package com.bluesoft.todoapp.controller;
 
+import com.bluesoft.todoapp.model.Task;
 import com.bluesoft.todoapp.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @RepositoryRestController
 class TaskController {
@@ -18,9 +23,18 @@ class TaskController {
         this.repository = repository;
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/tasks")
-    ResponseEntity<?> readAllTasks(){
+    @RequestMapping(method = RequestMethod.GET, path = "/tasks",
+            params = {"!sort","!page","!size"})
+    ResponseEntity<List<Task>> readAllTasks(){
         LOGGER.warn("Exposing all the tasks");
         return ResponseEntity.ok(repository.findAll());
     }
+
+    @GetMapping("/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable page){
+        LOGGER.warn("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
+
+
 }
