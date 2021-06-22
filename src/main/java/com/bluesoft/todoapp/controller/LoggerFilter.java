02a24790f6e8f87f7b0a3067d51988ec2860a062
janusz <jstolorz@gmail.com>
2021-06-22 17:46:57 +0200

    Adding filter to http request

---
+++ src/main/java/com/bluesoft/todoapp/controller/LoggerFilter.java
@@ -0,0 +1,37 @@
+package com.bluesoft.todoapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+
+@Component
+class LoggerFilter implements Filter {
+
+    Logger logger = LoggerFactory.getLogger(LoggerFilter.class);
+
+    @Override
+    public void init(final FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+         if(request instanceof HttpServletRequest){
+             var httpRequest = (HttpServletRequest) request;
+             logger.info("[doFilter] " + httpRequest.getMethod() + " " + httpRequest.getRequestURI());
+         }
+         chain.doFilter(request,response);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+
+
+}
